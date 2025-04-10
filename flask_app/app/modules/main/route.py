3d844a36b08e799fff10ff94440f327a4dd114e6
KrzysztofZdebski <km.zdebski@gmail.com>
2025-04-10 22:39:29 +0200

    f

--- flask_app/app/modules/main/route.py
+++ flask_app/app/modules/main/route.py
@@ -6,22 +6,8 @@ main_bp = Blueprint('main', __name__)
 main_controller = MainController()
 @main_bp.route('/', methods=['GET'])
 def index():
-    """ Example endpoint with simple greeting.
-    ---
-    tags:
-      - Example API
-    responses:
-      200:
-        description: A simple greeting
-        schema:
-          type: object
-          properties:
-            data:
-              type: object
-              properties:
-                message:
-                  type: string
-                  example: "Hello World!"
+    """ 
+    file: example.yml
     """
     result=main_controller.index()
     return make_response(jsonify(data=result))
